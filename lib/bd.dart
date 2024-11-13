1d9d9582b620b6e88bfc5f54b457f4dd551a138c
CarlosMunozBarco <carlosbarco0589@gmail.com>
2024-11-13 12:49:42 +0100

    Codigo de la base de datos comentado

--- lib/bd.dart
+++ lib/bd.dart
@@ -5,17 +5,39 @@ import 'package:proyecto/Decrypt.dart';
 import 'package:path/path.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
+
+
+/*
+Clase ColegioDatabase
+Almacena la base de datos de la aplicación
+
+
+*/
 class ColegioDatabase{
 
+    /*
+        Declaramos la instancia de la base de datos,
+        que se usarára cada vez que necesitemos acceder a ella.
+
+    */
 	static final ColegioDatabase instance = ColegioDatabase._init();
 
 	static Database? _database;
 	
 	ColegioDatabase._init();
 
+    /*
+        Declaramos los nombres de las tablas que usará la aplicación
+    */
 	final String tablaStudents = 'students';
 	final String tablaImgCode = 'imgCode';
 	final String tablaDecrypt = 'decrypt';
+
+    /*
+        Metodo inherente a las bases de datos sqlite que devuelve
+        la base de datos en caso de estar creada y la crea
+        en caso contrario
+    */
 
 	Future<Database> get database async {
 		if(_database != null) return _database!;
@@ -24,6 +46,13 @@ class ColegioDatabase{
 		return _database!;	
 	}
 	
+    /*
+        Método
+        @Nombre --> _initDB
+        @Funcion --> Inicializa la base de datos y la deja preparada para su posterior uso
+        @Argumentos
+            - filePath: ruta del archivo donde se guardará la base de datos
+    */
 	Future<Database> _initDB(String filePath) async {
     try {
       if (Platform.isLinux || Platform.isMacOS || Platform.isWindows) {
@@ -41,8 +70,22 @@ class ColegioDatabase{
     }
 	}
 
-	Future _onCreateDB(Database db, int version) async{
+    /*
+        Método
+        @Nombre --> _onCreateDB
+        @Funcion --> Se ejecuta cuando se crea la base de datos. Crea todas las tablas requeridas por la apliación
+        @Argumentos
+            - db: la base de datos sobre la que se crearán las tablas
+            - version: versión de la base de datos
+    */
 
+	Future _onCreateDB(Database db, int version) async{
+		
+
+		/*
+			Tabla Estudiantes.
+			Contendrá todos los ratos relativos a los estudiantes registrados en la aplicación
+    	*/
 		await db.execute('''
 			CREATE TABLE $tablaStudents(
 			user VARCHAR(30) PRIMARY KEY,
@@ -54,11 +97,15 @@ class ColegioDatabase{
 			interfaceIMG TINYINT(1) NOT NULL,
 			interfacePIC TINYINT(1) NOT NULL,
 			interfaceTXT TINYINT(1) NOT NULL,
-      diningRoomTask TINYINT(1) NOT NULL
+      		diningRoomTask TINYINT(1) NOT NULL
 			)
 			
 		''');
 
+		/*
+			Tabla imágenes.
+			Contendrá toda la información de las imágenes almacenadas en la aplicación
+    	*/
 		await db.execute('''
 			CREATE TABLE $tablaImgCode(
 			path VARCHAR(25) PRIMARY KEY,
@@ -66,6 +113,10 @@ class ColegioDatabase{
 			)
 		''');
 
+		/*
+			Tabla Decrypt.
+			Contiene la relación entre las imágenes y los alumnos
+    	*/
 		await db.execute('''
 			CREATE TABLE $tablaDecrypt(
 			user VARCHAR(30),
@@ -77,6 +128,14 @@ class ColegioDatabase{
 		''');
 	}
 
+    /*
+        Método
+        @Nombre --> loginStudent
+        @Funcion --> Comprueba que los datos introducidos corresponden a algun alumno registrado en la base de datos
+        @Argumentos
+            - user: usuario del alumno
+            - password: contraseña del alumno
+    */
 	Future<bool> loginStudent(String user, String password) async {
 		final db = await instance.database;
 
@@ -89,6 +148,15 @@ class ColegioDatabase{
 		return result.isNotEmpty;
 	}
 
+    /*
+        Método
+        @Nombre --> registerStudent
+        @Funcion --> Registra a un alumno en la base de datos
+        @Argumentos
+            - Student: objeto de la clase Student que contiene todos los datos necesarios para añadir un nuevo alumno a la tabla
+                       de estudiantes.
+    */
+
 	Future<bool> registerStudent(Student student) async {
 		final db = await instance.database;
 
@@ -100,6 +168,15 @@ class ColegioDatabase{
 			return false;
 		}
 	}
+
+    /*
+        Método
+        @Nombre --> asignLoginType
+        @Funcion --> Asigna un tipo de login a un alumno concreto
+        @Argumentos
+            - user: usuario al que se le realizará la modificación
+            - typePassword: tipo de contraseña que usará el alumno en cuestión
+    */
 
 	Future<bool> asignLoginType(String user, String typePassword) async {
 		final db = await instance.database;
@@ -119,6 +196,15 @@ class ColegioDatabase{
 		}
 	}
 
+    /*
+        Método
+        @Nombre --> modifyStudent
+        @Funcion --> Modifica los datos de un estudiante registrado en la base de datos
+        @Argumentos
+            - user: usuario del alumno al que se le realizará las modificaciónes
+            - data: dato del alumno que será modificado (user, password, etc...)
+            - newData: nuevo valor del dato modificado
+    */
 	Future<bool> modifyStudent(String user, String data, String newData) async{
 		final db = await instance.database;
 
@@ -136,7 +222,23 @@ class ColegioDatabase{
 			return false;
 		}
 	}
+    
+    /*
+        Método
+        @Nombre --> modifyCompleteStudent
+        @Funcion --> Actualiza todos los datos de un alumno registrado en la base de datos
+        @Argumentos
+            - user: usuario del alumno al que se le realizará las modificaciónes
+            - name: nombre
+            - surname: apellido
+            - password: contraseña
+            - photo: foto del alumno
+            - typePassword: tipo de contraseña que usará el alumno para iniciar al sesión
+            - interfaceIMG: determina si el alumno usará la interfaz basada en imágenes
+            - interfacePIC: determina si el alumno usará la interfaz basada en pictogramas
+            - intefaceTXT: determina si el alumno usará la interfaz basada en texto
 
+    */
 	Future<bool> modifyCompleteStudent(String user, String name, String? surname,String password, 
     String photo, String typePassword, int interfaceIMG, int interfacePIC, int interfaceTXT) async{
 		final db = await instance.database;
@@ -164,11 +266,24 @@ class ColegioDatabase{
 		}
 	}
 
+    /*
+        Método
+        @Nombre --> getAllStudents
+        @Funcion --> Devuelve todos los estudiantes de la base de datos
+    */
 	Future<List<Student>> getAllStudents() async {
 		final db = await instance.database;
 		final result = await db.query(tablaStudents);
 		return result.map((map) => Student.fromMap(map)).toList();
 	}
+
+    /*
+        Método
+        @Nombre --> getStudent
+        @Funcion --> Devuelve un estudiante en concreto en base a su usuario
+        @Argumentos
+            - user: usuario del alumno que será devuelto
+    */
 
 	Future<Student?> getStudent(String user) async {
 		final db = await instance.database;
@@ -184,12 +299,26 @@ class ColegioDatabase{
 		}
 	}
 
+    /*
+        Método
+        @Nombre --> getStudentPhotos
+        @Funcion --> Devuelve las fotos de todos los estudiantes
+    */
+
 	Future<List<String>> getStudentPhotos() async {
 		final db = await instance.database;
 		final result = await db.query(tablaStudents, columns: ['image']);
 		return result.map((map) => map['image'].toString()).toList();
 	}
 
+
+    /*
+        Método
+        @Nombre --> getImgCode
+        @Funcion --> Devuelve el código asignado a una imagen
+        @Argumentos
+            - path: ruta de la imagen en cuestión
+    */
 	Future<ImgCode?> getImgCode(String path) async {
 		final db = await instance.database;
 		final result = await db.query(
@@ -204,37 +333,67 @@ class ColegioDatabase{
 		}
 	}
 
+
+	/*
+        Método
+        @Nombre --> getAllImgCodes
+        @Funcion --> Devuelve el código de todas las imágenes de la base de datos
+    */
 	Future<List<ImgCode>> getAllImgCodes() async {
 		final db = await instance.database;
 		final result = await db.query(tablaImgCode);
 		return result.map((map) => ImgCode.fromMap(map)).toList();
 	}
 
-  Future<List<ImgCode>> getImgCodeFromFolder(String folder) async {
-    final db = await instance.database;
-    final result = await db.query(
-      tablaImgCode,
-      where: 'path LIKE ?',
-      whereArgs: ['%$folder%']
-    );
-    return result.map((map) => ImgCode.fromMap(map)).toList();
-  }
 
-  Future<void> insertDecryptEntries(String user, List<ImgCode> images) async {
-    final db = await instance.database;
+	/*
+        Método
+        @Nombre --> getImgCodeFromFolder
+        @Funcion --> Devuelve los códigos de las imagenes de una carpeta
+        @Argumentos
+            - folder: nombre de la carpeta que contiene las imágenes
+    */
+	Future<List<ImgCode>> getImgCodeFromFolder(String folder) async {
+		final db = await instance.database;
+		final result = await db.query(
+		tablaImgCode,
+		where: 'path LIKE ?',
+		whereArgs: ['%$folder%']
+		);
+		return result.map((map) => ImgCode.fromMap(map)).toList();
+	}
 
-    for (var imgCode in images) {
-      await db.insert(
-        tablaDecrypt,
-        {
-          'user': user,
-          'path': imgCode.path,
-        },
-        conflictAlgorithm: ConflictAlgorithm.ignore, // Evita errores si ya existe la tupla
-      );
-    }
-  }
 
+	/*
+        Método
+        @Nombre --> insertDecryptEntries
+        @Funcion --> Permite asignar a un alumno las imágenes que usará para iniciar sesión
+        @Argumentos
+            - user: el usuario del alumno al que se le asignarán las imágenes
+			- images: las imágenes que usará dicho usuario para iniciar sesión
+    */
+	Future<void> insertDecryptEntries(String user, List<ImgCode> images) async {
+		final db = await instance.database;
+
+		for (var imgCode in images) {
+			await db.insert(
+				tablaDecrypt,
+				{
+				'user': user,
+				'path': imgCode.path,
+				},
+				conflictAlgorithm: ConflictAlgorithm.ignore, // Evita errores si ya existe la tupla
+			);
+		}
+	}
+
+	/*
+        Método
+        @Nombre --> getDecryptByStudent
+        @Funcion --> Devuelve las imágenes que usa un alumno para iniciar sesión
+        @Argumentos
+            - user: el usuario del alumno del que se obtendran las imágenes
+    */
 	Future<List<Decrypt>> getDecryptsByStudent(String user) async {
 		final db = await instance.database;
 		final result = await db.query(
@@ -245,43 +404,79 @@ class ColegioDatabase{
 		return result.map((map) => Decrypt.fromMap(map)).toList();
 	}
 
+	/*
+        Método
+        @Nombre --> getAllDecrypts
+        @Funcion --> Devuelve todas las imágenes almacenadas en la base de datos
+    */
 	Future<List<Decrypt>> getAllDecrypts() async {
 		final db = await instance.database;
 		final result = await db.query(tablaDecrypt);
 		return result.map((map) => Decrypt.fromMap(map)).toList();
 	}
 
-  Future<bool> userIsValid(String user) async {
-    final db = await instance.database;
-    final result = await db.query(
-      tablaStudents,
-      where: 'user = ?',
-      whereArgs: [user],
-    );
-    return result.isEmpty;
-  }
-
-  Future<String> getCodeImgCode(String path) async{
-    final db = await instance.database;
-    final result = await db.query(
-      tablaImgCode,
-      where: 'path = ?',
-      whereArgs: [path],
-    );
-    return result.first['code'].toString();
-  }
-  
-  Future<bool> insertImgCode(String path, String code) async{
+	
+	/*
+        Método
+        @Nombre --> userIsValid
+        @Funcion --> Comprueba que un usuario en concreto exista en la base de datos
+        @Argumentos
+            - user: el usuario del alumno
+    */
+	Future<bool> userIsValid(String user) async {
 		final db = await instance.database;
-		try {
-			await db.insert(tablaImgCode, {'path': path, 'code': code});
-			return true;
-		} catch (e) {
-			print("Error al insertar el código de la imagen: $e");
-			return false;
-		}
+		final result = await db.query(
+		tablaStudents,
+		where: 'user = ?',
+		whereArgs: [user],
+		);
+		return result.isEmpty;
 	}
 
+
+	/*
+        Método
+        @Nombre --> getImgCode
+        @Funcion --> Devuelve el código asociado a una imagen en concreto
+        @Argumentos
+            - path: la ruta de la imágen
+    */
+	Future<String> getCodeImgCode(String path) async{
+		final db = await instance.database;
+		final result = await db.query(
+		tablaImgCode,
+		where: 'path = ?',
+		whereArgs: [path],
+		);
+		return result.first['code'].toString();
+	}
+	
+	/*
+        Método
+        @Nombre --> insertImgCode
+        @Funcion --> Inserta una imágen con su código en la base de datos
+        @Argumentos
+            - path: ruta de la imagen
+			- code: código asignado a la imagen
+    */
+	Future<bool> insertImgCode(String path, String code) async{
+			final db = await instance.database;
+			try {
+				await db.insert(tablaImgCode, {'path': path, 'code': code});
+				return true;
+			} catch (e) {
+				print("Error al insertar el código de la imagen: $e");
+				return false;
+			}
+		}
+
+	/*
+        Método
+        @Nombre --> getImgCodePath
+        @Funcion --> Devuelve la ruta de una imagen dada su código asignado
+        @Argumentos
+            - code: el código asignado a la imagen que queremos obtener
+    */
 	Future<String> getImgCodePath(String code) async{
 		final db = await instance.database;
 		final result = await db.query(
@@ -292,6 +487,13 @@ class ColegioDatabase{
 		return result.first['path'].toString();
 	}
 
+	/*
+        Método
+        @Nombre --> imgCodePathCount
+        @Funcion --> devuelve el número de imágenes que tienen el mismo path
+        @Argumentos
+            - path: la ruta que deberan compartir las imágenes
+    */
 	Future<int> imgCodePathCount(String path) async{
 		final db = await instance.database;
 		final result = await db.query(
--- lib/bd_utils.dart
+++ lib/bd_utils.dart
@@ -5,14 +5,40 @@ import 'ImgCode.dart';
 String user = 'admin';
 String password = 'admin';
 
+
+/*
+    Función
+    @Nombre --> loginAdmin
+    @Funcion --> Comprueba que los datos introducidos correspondan al administrador
+    @Argumentos
+        - user: usuario introducido
+        - password: contraseña introducida
+*/
 bool loginAdmin(String user, String password) {
   return user == 'admin' && password == 'admin';
 }
 
+/*
+    Función
+    @Nombre --> loginStudent
+    @Funcion --> Comprueba que los datos introducidos correspondan a algun alumno registrado
+    @Argumentos
+        - user: el usuario del alumno
+        - password: contraseña del alumno
+*/
 Future<bool> loginStudent(String user, String password) async {
 	return await ColegioDatabase.instance.loginStudent(user, password);
 }
 
+/*
+    Función
+    @Nombre --> login
+    @Funcion --> Comprueba que los datos introducidos correspondan o bien al administrador 
+                 o bien a algun alumno registrado en el sistema
+    @Argumentos
+        - user: usuario introducido
+        - password: contraseña introducida
+*/
 Future<bool> login(String user, String password) async {
   if(user == "admin"){
     return loginAdmin(user, password);
@@ -22,6 +48,23 @@ Future<bool> login(String user, String password) async {
   }
 }
 
+
+/*
+    Función
+    @Nombre --> registerStudent
+    @Funcion --> Registra un alumno en la base de datos
+    @Argumentos
+        - user: usuario del alumno que será registrado
+        - name: nombre
+        - surname: apellido
+        - password: contraseña
+        - image: foto del alumno
+        - typePassword: tipo de contraseña que usará el alumno para iniciar al sesión
+        - interfaceIMG: determina si el alumno usará la interfaz basada en imágenes
+        - interfacePIC: determina si el alumno usará la interfaz basada en pictogramas
+        - intefaceTXT: determina si el alumno usará la interfaz basada en texto
+
+*/
 Future<bool> registerStudent(String user, name, String surname, String password, 
   String image, String typePassword, int interfaceIMG,
 	int interfacePIC, int interfaceTXT) async {
@@ -38,35 +81,109 @@ Future<bool> registerStudent(String user, name, String surname, String password,
 
 }
 
+
+/*
+    Función
+    @Nombre --> asignLoginType
+    @Funcion --> asigna a un alumno concreto un tipo de inicio de sesión
+    @Argumentos
+        - user: usuario del alumno al que se le realizará la modificación
+        - typePassword: tipo de inicio de sesión que será asignado al alumno
+*/
 Future<bool> asignLoginType(String user, String typePassword) async {
 	return await ColegioDatabase.instance.asignLoginType(user, typePassword);
 }
 
+/*
+    Función
+    @Nombre --> createStudentImgCodePassword
+    @Funcion --> Permite asignar a un alumno las imágenes que usará para iniciar sesión
+    @Argumentos
+        - user: el usuario del alumno al que se le asignarán las imágenes
+        - images: las imágenes que usará dicho usuario para iniciar sesión
+*/
 Future<void> createStudentImgCodePassword(String user, List<ImgCode> images) async {
   await ColegioDatabase.instance.insertDecryptEntries(user, images);
 }
 
+
+/*
+    Función
+    @Nombre --> getImgCodeFromFolder
+    @Funcion --> Devuelve los códigos de las imagenes de una carpeta
+    @Argumentos
+        - folder: nombre de la carpeta que contiene las imágenes
+*/
 Future<List<ImgCode>> getImgCodeFromFolder(String folder) async {
   return await ColegioDatabase.instance.getImgCodeFromFolder(folder);
 }
 
+
+/*
+    Función
+    @Nombre --> modifyNameStudent
+    @Funcion --> Modifica el nombre de un estudiante concreto
+    @Argumentos
+        - user: usuario del alumno cuyo nombre será modificado
+        - newName: el nuevo nombre del alumno
+*/
 Future<bool> modifyNameStudent(String user, String newName) async {
 	return await ColegioDatabase.instance.modifyStudent(user, "name", newName);
 }
 
+/*
+    Función
+    @Nombre --> modifyPasswordStudent
+    @Funcion --> Modifica la contraseña de un estudiante concreto
+    @Argumentos
+        - user: usuario del alumno cuya contraseña será modificada
+        - newPassword: la nueva contraseña del alumno
+*/
 Future<bool> modifyPasswordStudent(String user, String newPassword) async {
 	return await ColegioDatabase.instance.modifyStudent(user, "password", newPassword);
 }
 
+/*
+    Método
+    @Nombre --> modifyCompleteStudent
+    @Funcion --> Actualiza todos los datos de un alumno registrado en la aplicación
+    @Argumentos
+        - user: usuario del alumno al que se le realizará las modificaciónes
+        - name: nombre
+        - surname: apellido
+        - password: contraseña
+        - photo: foto del alumno
+        - typePassword: tipo de contraseña que usará el alumno para iniciar al sesión
+        - interfaceIMG: determina si el alumno usará la interfaz basada en imágenes
+        - interfacePIC: determina si el alumno usará la interfaz basada en pictogramas
+        - intefaceTXT: determina si el alumno usará la interfaz basada en texto
+
+*/
 Future<bool> modifyCompleteStudent(String user, String name, String? surname, String password, 
   String photo, String typePassword, int interfaceIMG, int interfacePIC, int interfaceTXT) async {
   return await ColegioDatabase.instance.modifyCompleteStudent(user, name, surname, password, photo, typePassword, interfaceIMG, interfacePIC, interfaceTXT);
 }
 
+
+/*
+    Método
+    @Nombre --> userIsValid
+    @Funcion --> Comprueba que un usuario en concreto exista en la aplicación
+    @Argumentos
+        - user: el usuario del alumno
+*/
 Future<bool> userIsValid(String user) async {
   return await ColegioDatabase.instance.userIsValid(user);
 }
 
+
+/*
+    Método
+    @Nombre --> userFormat
+    @Funcion --> Comprueba que un nombre de usuario esté en un formato correcto
+    @Argumentos
+        - texto: el usuario cuyo formato será comprobado
+*/
 bool userFormat(String texto) {
   final regex = RegExp(r'^[a-zA-Z0-9]+$');
   return regex.hasMatch(texto);
